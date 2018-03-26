c6f85eed7c8bbf39e3ee57a7dcb711612668c159
Ingvaras Merkys <ingvaras.merkys@westernunion.com>
2018-03-26 14:40:05 +0300

    read matrix from Octave text file

--- Vargas.hs
+++ Vargas.hs
@@ -1,44 +1,16 @@
 import System.Environment
 import Data.List.Split
-import Text.Regex.TDFA
+import Data.Matrix
 
-parseCSV :: String -> [(String, String, String)]
-parseCSV strs = [y | Just y <- [parseCSVLine x | x <- splitOn "\n" strs]]
+parseMatrix :: String -> Matrix Int
+parseMatrix str = fromLists [y | Just y <- [parseRow x | x <- splitOn "\n" str]]
 
-parseCSVLine :: String -> Maybe (String, String, String)
-parseCSVLine str = parseParse x
-    where x = str =~ "^([^,]*),([^,]*),([^,]*)$" :: [[String]]
-
-parseParse :: [[String]] -> Maybe (String, String, String)
-parseParse [] = Nothing
-parseParse [[]] = Nothing
-parseParse [(x:xs)] = Just $ tuplify3(xs)
-
-tuplify3  :: [a] -> (a, a, a)
-tuplify3 (x:y:z:[]) = (x, y, z)
-tuplify3 _ = error "asdfasdf"
-
-printTuples3 :: [(String, String, String)] -> IO ()
-printTuples3 [] = return ()
-printTuples3 (t:pls) = do
-                        printTuple3 t
-                        printTuples3 pls
-
-printTuple3 :: (String, String, String) -> IO ()
-printTuple3 (x, y, z) = do
-    putStrLn (x ++ "GEKAS" ++ y ++ "GEKAS" ++ z)
+parseRow :: String -> Maybe [Int]
+parseRow ('#':xs) = Nothing
+parseRow []       = Nothing
+parseRow str      = Just $ map (read :: String -> Int) $ filter (\x -> x /= []) (splitOn " " str)
 
 main = do
    f <- getArgs
    s <- readFile $ head f
-   printTuples3 $ parseCSV s
-
-1/m sum to m (Theta1 * )
--- Xtrain
--- ytrain
--- Xval
--- yval
--- Xtest
--- ytest
-
-function
+   putStrLn $ prettyMatrix $ parseMatrix s
