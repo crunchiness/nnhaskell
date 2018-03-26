bb5b00206908e02c84b0f9742e022fcfd6585d94
Ingvaras Merkys <ingvaras.merkys@westernunion.com>
2018-03-26 15:06:30 +0300

    parseMatrix polymorphic

--- Vargas.hs
+++ Vargas.hs
@@ -2,15 +2,15 @@ import System.Environment
 import Data.List.Split
 import Data.Matrix
 
-parseMatrix :: String -> Matrix Int
+parseMatrix :: Read a => String -> Matrix a
 parseMatrix str = fromLists [y | Just y <- [parseRow x | x <- splitOn "\n" str]]
 
-parseRow :: String -> Maybe [Int]
+parseRow :: Read a => String -> Maybe [a]
 parseRow ('#':xs) = Nothing
 parseRow []       = Nothing
-parseRow str      = Just $ map (read :: String -> Int) $ filter (\x -> x /= []) (splitOn " " str)
+parseRow str      = Just $ map read $ filter (\x -> x /= []) (splitOn " " str)
 
 main = do
    f <- getArgs
    s <- readFile $ head f
-   putStrLn $ prettyMatrix $ parseMatrix s
+   putStrLn $ prettyMatrix $ (parseMatrix s :: Matrix Int)
