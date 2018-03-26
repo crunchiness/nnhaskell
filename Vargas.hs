6ac0e47e6df192b66bbd2f906f0f4c273eec4eff
Ingvaras Merkys <ingvaras.merkys@westernunion.com>
2018-03-26 15:25:06 +0300

    removed warnings

--- Vargas.hs
+++ Vargas.hs
@@ -6,10 +6,11 @@ parseMatrix :: Read a => String -> Matrix a
 parseMatrix str = fromLists [y | Just y <- [parseRow x | x <- splitOn "\n" str]]
 
 parseRow :: Read a => String -> Maybe [a]
-parseRow ('#':xs) = Nothing
-parseRow []       = Nothing
-parseRow str      = Just $ map read $ filter (\x -> x /= []) (splitOn " " str)
+parseRow ('#':_) = Nothing
+parseRow []      = Nothing
+parseRow str     = Just $ map read $ filter (\x -> x /= []) (splitOn " " str)
 
+main :: IO ()
 main = do
    f <- getArgs
    s <- readFile $ head f
