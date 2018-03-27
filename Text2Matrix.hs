d8ca3ff0f1a2b23cd51f84df41786baf5bfc48c3
Ingvaras Merkys <ingvaras.merkys@westernunion.com>
2018-03-27 16:06:59 +0300

    minElem and maxElem functions

--- Text2Matrix.hs
+++ Text2Matrix.hs
@@ -10,10 +10,35 @@ parseRow ('#':_) = Nothing
 parseRow []      = Nothing
 parseRow str     = Just $ map read $ filter (\x -> x /= []) (splitOn " " str)
 
+maxElem :: Ord a => Matrix a -> a
+maxElem m = mostElemFrom (>) m rows cols lastElem
+            where
+                lastElem = m ! (rows, cols)
+                rows = nrows m
+                cols = ncols m
+
+minElem :: Ord a => Matrix a -> a
+minElem m = mostElemFrom (<) m rows cols lastElem
+            where
+                lastElem = m ! (rows, cols)
+                rows = nrows m
+                cols = ncols m
+
+mostElemFrom :: Ord a => (a -> a -> Bool) -> Matrix a -> Int -> Int -> a -> a
+mostElemFrom cmp m i j cmost | i == 0      = cmost
+                             | j == 0      = mostElemFrom cmp m (i - 1) cols cmost
+                             | cmp c cmost = mostElemFrom cmp m i (j - 1) c
+                             | otherwise   = mostElemFrom cmp m i (j - 1) cmost
+                             where
+                                 c = m ! (i, j)
+                                 cols = ncols m
+
 main :: IO ()
 main = do
    f <- getArgs
    s <- readFile $ head f
-   let matrix = parseMatrix s :: Matrix Int
-   putStrLn $ show $ nrows $ matrix
-   putStrLn $ show $ ncols $ matrix
+   let m = parseMatrix s :: Matrix Int
+   putStrLn $ "Rows: " ++ (show $ nrows $ m)
+   putStrLn $ "Cols: " ++ (show $ ncols $ m)
+   putStrLn $ "Min: " ++ (show $ minElem $ m)
+   putStrLn $ "Max: " ++ (show $ maxElem $ m)
