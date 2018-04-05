9af077e131fed55f71ff58c55eaad691b2d57bc1
Ingvaras Merkys <ingvaras.merkys@westernunion.com>
2018-04-05 16:19:34 +0300

    Matrix2Image displays the image

--- src/Matrix2Image.hs
+++ src/Matrix2Image.hs
@@ -1,56 +1,63 @@
 module Matrix2Image where
 
-import Data.Array.MArray
+import Data.Array.MArray (writeArray)
 import Data.Matrix
 import Data.Word
 
 import Graphics.UI.Gtk (eventWindow)
-import Graphics.UI.Gtk.Abstract.Widget (widgetGetDrawWindow, widgetShowAll)
-import Graphics.UI.Gtk.General.General (initGUI, mainGUI)
+import Graphics.UI.Gtk.Abstract.Widget (onDestroy, widgetGetDrawWindow, widgetSetSizeRequest, widgetShowAll)
+import Graphics.UI.Gtk.General.General (initGUI, mainGUI, mainQuit, timeoutAdd)
 import Graphics.UI.Gtk.Gdk.Drawable
-import Graphics.UI.Gtk.Gdk.GC
+import Graphics.UI.Gtk.Gdk.GC (gcNew)
 import Graphics.UI.Gtk.Gdk.Pixbuf
 import Graphics.UI.Gtk.Windows.Window (windowNew)
 
-lol :: IO Pixbuf
-lol = pixbufNew ColorspaceRgb False 8 32 32
+createPixbuf :: Int -> Int -> IO Pixbuf
+createPixbuf w h = pixbufNew ColorspaceRgb False 8 w h
 
-setRGBA :: Pixbuf -> Int -> Int -> (Word8, Word8, Word8, Word8) -> IO ()
-setRGBA pb x y (r, g, b, a) = do
+setRGB :: Pixbuf -> Int -> Int -> (Word8, Word8, Word8) -> IO ()
+setRGB pb x y (r, g, b) = do
     ps <- pixbufGetPixels pb :: IO (PixbufData Int Word8)
     row <- pixbufGetRowstride pb
-    writeArray ps (y*row + 4*x + 0) r
-    writeArray ps (y*row + 4*x + 1) g
-    writeArray ps (y*row + 4*x + 2) b
-    writeArray ps (y*row + 4*x + 3) a
+    writeArray ps (y * row + 3 * x + 0) r
+    writeArray ps (y * row + 3 * x + 1) g
+    writeArray ps (y * row + 3 * x + 2) b
 
 setPixels :: RealFrac a => Pixbuf -> Matrix a -> IO [()]
 setPixels pb m = do
-                     sequence [ f x y $ m ! (x, y) | y <- [0 .. (nrows m) - 1], x <- [0 .. (ncols m) - 1] ]
+                     sequence [ f (x - 1) (y - 1) $ m ! (x, y) | y <- [1 .. (nrows m)], x <- [1 .. (ncols m)] ]
                      where
                          f x y val = do
                              let r = (round val :: Word8)
-                             setRGBA pb x y (128, 128, 200, 128)
+                             setRGB pb x y (r, r, r)
 
 drawSmth :: RealFrac a => Matrix a -> IO ()
 drawSmth m = do
-               initGUI
-               w <- windowNew
-               widgetShowAll w
-               mainGUI
-               dw <- widgetGetDrawWindow w
-               gc <- gcNew dw
-               pb <- lol
-               setPixels pb m
-               drawPixbuf dw gc pb srcX srcY destX destY srcWidth srcHeight dither xDither yDither
-               mainGUI
-               where
-                   srcX = 0
-                   srcY = 0
-                   destX = 0
-                   destY = 0
-                   srcWidth = -1
-                   srcHeight = -1
-                   dither = RgbDitherNone
-                   xDither = 0
-                   yDither = 0
+    initGUI
+    let w = ncols m; h = nrows m
+    window <- windowNew
+    onDestroy window mainQuit
+    widgetSetSizeRequest window w h
+    widgetShowAll window
+    dw <- widgetGetDrawWindow window
+    gc <- gcNew dw
+    pb <- createPixbuf (ncols m) (nrows m)
+    setPixels pb m
+
+    let draw = do { drawPixbuf dw gc pb srcX srcY destX destY srcWidth srcHeight dither xDither yDither
+                  ; return True }
+
+    timeoutAdd draw 500
+
+    widgetShowAll window
+    mainGUI
+    where
+        srcX = 0
+        srcY = 0
+        destX = 0
+        destY = 0
+        srcWidth = -1
+        srcHeight = -1
+        dither = RgbDitherNone
+        xDither = 0
+        yDither = 0
